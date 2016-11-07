4af3b5db41616c1b83f32d0e72c1ca2100f5d14e
Carlos Matheu Armengol <info@plastikaweb.com>
2016-11-07 11:17:29 +0100

    pagination lessons for course

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,27 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import {CoursesService} from '../shared/courses.service';
-import {Observable} from 'rxjs';
-import {Lesson} from '../shared/lesson';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../shared/course';
+import {Component, OnInit} from '@angular/core';
+import {CoursesService} from "../shared/courses.service";
+import {Lesson} from "../shared/lesson";
+import {Observable} from "rxjs/Rx";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Course} from "../shared/course";
 
-@Component({
-  selector: 'app-course-detail',
-  templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.css']
-})
+
+@Component( {
+    selector: 'app-course-detail',
+    templateUrl: './course-detail.component.html',
+    styleUrls: ['./course-detail.component.css']
+} )
 export class CourseDetailComponent implements OnInit {
-  lessons: Observable<Lesson[]>;
-  course: Observable<Course>;
 
-  constructor(private route: ActivatedRoute,
-              private coursesService: CoursesService) { }
+    course$: Observable<Course>;
+    lessons: Lesson[];
 
-  ngOnInit() {
-    const courseUrl = this.route.snapshot.params['id'];
-    this.course = this.coursesService.findCourseByUrl(courseUrl);
+    courseUrl: string;
 
-    this.lessons = this.coursesService.findLessonsForCourse(courseUrl);
-  }
+    constructor(private router: Router,
+                private route: ActivatedRoute,
+                private coursesService: CoursesService) {
+
+
+    }
+
+    ngOnInit() {
+
+        this.courseUrl = this.route.snapshot.params['id'];
+
+        this.course$ = this.coursesService.findCourseByUrl( this.courseUrl );
+
+        const lessons$ = this.coursesService.loadFirstLessonsPage( this.courseUrl, 3 );
+
+        lessons$.subscribe( lessons => this.lessons = lessons );
+
+    }
+
+    next() {
+
+        this.coursesService.loadNextPage(
+            this.courseUrl,
+            this.lessons[this.lessons.length - 1].$key,
+            3
+        )
+            .subscribe( lessons => this.lessons = lessons );
+
+
+    }
+
+
+    previous() {
+
+        this.coursesService.loadPreviousPage(
+            this.courseUrl,
+            this.lessons[0].$key,
+            3
+        )
+            .subscribe( lessons => this.lessons = lessons );
+
+    }
+
+    navigateToLesson(lesson: Lesson) {
+
+        this.router.navigate( ['lessons', lesson.url] );
+
+    }
+
 
 }
+
+
+
+
+
+
--- src/app/shared/courses.service.ts
+++ src/app/shared/courses.service.ts
@@ -3,9 +3,12 @@ import {Observable} from 'rxjs';
 import {Course} from './course';
 import {AngularFireDatabase} from 'angularfire2';
 import {Lesson} from './lesson';
+import {FirebaseListFactoryOpts} from 'angularfire2/interfaces';
 
 @Injectable()
 export class CoursesService {
+    currentCourse: Course;
+
     constructor(private db: AngularFireDatabase) {
     }
 
@@ -15,25 +18,70 @@ export class CoursesService {
     }
 
     findCourseByUrl(courseUrl: string): Observable<Course> {
-        return this.db.list( 'courses', {
-            query: {
-                orderByChild: 'url',
-                equalTo: courseUrl
-            }
-        } )
-            .map( results => results[0] );
+        return this.db.list( 'courses')
+            .map( results => results[0]);
     }
 
-    findLessonKeysPerCourseUrl(courseUrl: string) : Observable<string[]> {
-        return this.findCourseByUrl(courseUrl)
-            .switchMap( course => this.db.list( 'lessonsPerCourse/' + course.$key ) )
-            .map(lspc => lspc.map(lpc => lpc.$key));
+    findLessonKeysPerCourseUrl(courseUrl: string,
+                               query: FirebaseListFactoryOpts = {}): Observable<string[]> {
+        return this.findCourseByUrl( courseUrl )
+            .switchMap( course => {
+                console.log(course);
+                this.currentCourse = course || this.currentCourse;
+                return this.db.list( 'lessonsPerCourse/' + this.currentCourse.$key, query )
+            } )
+            .map( lspc => lspc.map( lpc => lpc.$key ) );
+    }
+
+    findLessonsForLessonKeys(lessonKeys: Observable<string[]>): Observable<Lesson[]> {
+        return lessonKeys
+            .map( lspc => lspc.map( lessonKey => this.db.object( 'lessons/' + lessonKey ) ) )
+            .flatMap( fbojs => Observable.combineLatest( fbojs ) );
     }
 
     findLessonsForCourse(courseUrl: string): Observable<Lesson[]> {
-        return this.findLessonKeysPerCourseUrl(courseUrl)
-            .map( lspc => lspc.map( lessonKey => this.db.object( 'lessons/' + lessonKey ) ) )
-            .flatMap( fbojs => Observable.combineLatest( fbojs ) );
+        return this.findLessonsForLessonKeys( this.findLessonKeysPerCourseUrl( courseUrl ) );
+    }
+
+    loadFirstLessonsPage(courseUrl: string, items: number): Observable<Lesson[]> {
+
+        const firstPageLessonKeys = this.findLessonKeysPerCourseUrl( courseUrl, {
+            query: {
+                limitToFirst: items
+            }
+        } );
+
+        return this.findLessonsForLessonKeys( firstPageLessonKeys );
+    }
+
+    loadNextPage(courseUrl: string,
+                 lessonKey: string,
+                 items: number): Observable<Lesson[]> {
+        const lessonKeys = this.findLessonKeysPerCourseUrl( courseUrl, {
+            query: {
+                orderByKey: true,
+                startAt: lessonKey,
+                limitToFirst: items + 1
+            }
+        } );
+
+        return this.findLessonsForLessonKeys( lessonKeys )
+            .map( lessons => lessons.slice( 1, lessons.length ) );
+    }
+
+    loadPreviousPage(courseUrl: string,
+                     lessonKey: string,
+                     items: number): Observable<Lesson[]> {
+        const lessonKeys = this.findLessonKeysPerCourseUrl( courseUrl, {
+            query: {
+                orderByKey: true,
+                endAt: lessonKey,
+                limitToLast: items + 1
+            }
+        } );
+
+        return this.findLessonsForLessonKeys( lessonKeys )
+            .map( lessons => lessons.slice( 0,  lessons.length - 1) );
     }
 
 }
